86d100e1872b6ae583dd60137db27df8c20a0d73
FRONT LIANG (ADEV-DEV-CS/ZHA) <LIANGFR2@oocl.com>
2021-01-11 16:51:36 +0800

    set express view engine and create index.html

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+const ejs = require('ejs')
+const path = require('path')
 const express = require('express')
+
 const app = express()
+var router = express.Router()
 
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', function(req, res){
-  res.send('Welcome to my app!')
+router.get('/', function(req, res, next){
+  res.render('index.html', {content: 'Welcome to my app!'})
 })
+app.use('/app', router)
+// http://host:port/app  -> index.html
+
+// app.get('/', function(req, res){
+//   res.send('Welcome to my app!')
+// })
 
 const port = 18080
 app.listen(port, () => console.log('App listen on ', port))
+
+
+
+// console.log(__dirname);
